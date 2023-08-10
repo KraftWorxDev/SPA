cad396229caab5053c007b26762276a2975c79f5
maurice-cubicit <maurice@cubicit.co.za>
2023-08-10 16:13:54 +0200

    feat: added solid js to venus section

--- src/components/general/Venus.tsx
+++ src/components/general/Venus.tsx
@@ -1,18 +1,47 @@
 import { Component } from "solid-js";
-// import Venus1Img from "../../assets/img";
+import "solid-slider/slider.css";
+import { Slider, createSlider, SliderProvider, SliderButton} from "solid-slider";
+import Venus1Img from "../../assets/img/service1.png";
 // import Venus2Img from "../../assets/img";
 // import Venus3Img from "../../assets/img";
-import { IoArrowDownOutline } from 'solid-icons/io';
+import { IoArrowBackOutline, IoArrowForwardOutline } from 'solid-icons/io';
 
 const Venus:Component = () => {
     return (
         <div class="w-full py-16">
             <div class="w-10/12 m-auto">
-                <h1 class="text-5xl font-semibold text-center pb-16"> 
+                <h1 class="text-5xl text-gray-600  text-center pb-16"> 
                     Venus
                 </h1>
-                <div class="w-full flex flex-wrap sm:flex-nowrap gap-5">
-                    
+                <div class="w-full gap-5 relative">
+                        <SliderProvider>
+                            <div class="w-11/12 flex m-auto">
+                            <Slider options={{ loop: true }}>
+                                <div class="w-10/12 m-auto h-[65vh] flex flex-wrap">
+                                    <div class="w-3/4 flex justify-center">
+                                        <img 
+                                            src={Venus1Img} 
+                                            alt="venu one image" 
+                                            class="rounded-md h-[60vh] shadow"
+                                        />
+                                    </div>
+                                    <div class="w-1/4 flex">
+                                        <div class="my-auto px-4 font-thin text-xl">
+                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti repellat sint quas illum cum? Deleniti nesciunt est quis quos quam!
+                                        </div>
+                                    </div>
+                                </div>
+                            </Slider>
+                            </div>
+                            <div class="w-full flex absolute text-gray-400 top-56 justify-between">
+                                <SliderButton prev class="text-left">
+                                    <IoArrowBackOutline class="text-3xl" />
+                                </SliderButton>
+                                <SliderButton next class="text-right">
+                                    <IoArrowForwardOutline class="text-3xl" />
+                                </SliderButton>
+                            </div>
+                        </ SliderProvider>
                 </div>
             </div>
         </div>
