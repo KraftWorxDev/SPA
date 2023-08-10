ded6e51cb8c101b849cf1dd5bee71693b9d29e01
maurice-cubicit <maurice@cubicit.co.za>
2023-08-10 14:37:48 +0200

    fix: added a call to action button to about us

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
 import { Component } from "solid-js";
 import Hero from "../components/general/Hero";
+import About from "../components/general/About";
 
 const Home:Component = () => {
     return (
         <>
             <Hero />
+            <About />
         </>
     )
 }
