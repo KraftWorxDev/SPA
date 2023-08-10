930a5c03679205b7e28acb79eb13b725385d68e6
maurice-cubicit <maurice@cubicit.co.za>
2023-08-10 13:34:06 +0200

    feat:: added firebase with github actions

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import type { Component } from 'solid-js';
-
-import logo from './logo.svg';
-import styles from './App.module.css';
+import Nav from './components/navigation/Nav';
+import Footer from './components/navigation/Footer';
+import Home from './pages/Home';
 
 const App: Component = () => {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <Home />
+      <Footer />
+    </>
   );
 };
 
---
+++ src/components/navigation/Nav.tsx
@@ -0,0 +1,9 @@
+import { Component } from "solid-js";
+
+const Nav:Component = () => {
+    return (
+        <nav></nav>
+    )
+}
+
+export default Nav;
