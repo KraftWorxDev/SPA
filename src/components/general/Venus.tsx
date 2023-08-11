c331843a7d653ce74b762cacf7aa554dd14dd585
maurice-cubicit <maurice@cubicit.co.za>
2023-08-11 12:56:00 +0200

    fix: arrors on venu pictures

--- src/components/general/Venus.tsx
+++ src/components/general/Venus.tsx
@@ -9,24 +9,24 @@ import { IoArrowBackOutline, IoArrowForwardOutline } from 'solid-icons/io';
 const Venus:Component = () => {
     return (
         <div class="w-full py-16 scroll-mt-16" id="venu">
-            <div class="w-10/12 m-auto">
+            <div class="w-full lg:w-10/12 m-auto">
                 <h1 class="text-5xl text-gray-600  text-center pb-10 md:pb-16"> 
                     Our Available Venus
                 </h1>
                 <div class="w-full gap-5 relative">
                         <SliderProvider>
-                            <div class="w-11/12 flex m-auto">
+                            <div class="w-10/12 flex m-auto">
                             <Slider options={{ loop: true }}>
-                                <div class="w-10/12 m-auto h-[65vh] flex flex-wrap">
-                                    <div class="w-full md:w-3/4 flex justify-center">
+                                <div class="w-9/12 lg:w-10/12 m-auto h-[65vh] flex flex-wrap">
+                                    <div class="w-full lg:w-3/4 flex justify-center">
                                         <img 
                                             src={Venus1Img} 
                                             alt="venu one image" 
-                                            class="rounded-md md:h-[60vh] shadow"
+                                            class="rounded-md lg:h-[60vh] shadow"
                                         />
                                     </div>
-                                    <div class="w-full md:w-1/4 flex">
-                                        <div class="my-auto pr-4 font-thin text-xl">
+                                    <div class="w-full lg:w-1/4 flex">
+                                        <div class="my-auto pr-4 text-center lg:text-left font-thin text-xl">
                                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti repellat sint quas illum cum? Deleniti nesciunt est quis quos quam!
                                         </div>
                                     </div>
